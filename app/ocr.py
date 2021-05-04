c4b6aeb81deddfc10f34f234975bc8ddc531574a
vik03 <39328594+vik03@users.noreply.github.com>
2021-05-04 16:58:45 +0530

    Update ocr.py

--- app/ocr.py
+++ app/ocr.py
@@ -4,32 +4,12 @@ import sys
 
 
 async def read_image(img_path, lang='eng'):
-    """
-    Performs OCR on a single image
-
-    :img_path: str, path to the image file
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :text: str, converted text from image
-    """
-
     try:
         return pytesseract.image_to_string(img_path, lang=lang)
     except:
         return "[ERROR] Unable to process file: {0}".format(img_path)
 
 async def read_images_from_dir(dir_path, lang='eng', write_to_file=False):
-    """
-    Performs OCR on all images present in a directory
-
-    :dir_path: str, path to the directory of images
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :converted_text: dict, mapping of filename to converted text for each image
-    """
-
     converted_text = {}
     for file_ in os.listdir(dir_path):
         if file_.endswith(('png', 'jpeg', 'jpg', 'JPG')):
